da1df4a9481cac4436322b89cb0070d88d9d8959
islam0122 <duishobaevislam01@gmail.com>
2025-08-27 13:14:58 +0600

    day_03

--- day_03.py
+++ day_03.py
@@ -11,3 +11,20 @@ Remove the minimum --> 7kyu
 # print(remove_smallest([1,2,3,4,5]))
 
 
+"""
+Rock Paper Scissors! ---> 8kyu
+"""
+# def rps(p1, p2):
+#     if p1 == p2:
+#         return "Draw!"
+#
+#     rules = {
+#         "rock": "scissors",
+#         "scissors": "paper",
+#         "paper": "rock"
+#     }
+#
+#     if rules[p1] == p2:
+#         return "Player 1 won!"
+#     else:
+#         return "Player 2 won!"
