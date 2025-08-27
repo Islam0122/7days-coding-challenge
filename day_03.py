581ea082346486d4fd289dc7483c986358bda15f
islam0122 <duishobaevislam01@gmail.com>
2025-08-27 13:12:57 +0600

    day_03

---
+++ day_03.py
@@ -0,0 +1,13 @@
+"""
+Remove the minimum --> 7kyu
+"""
+# def remove_smallest(numbers):
+#     if not numbers:
+#         return []
+#     result = numbers.copy()
+#     result.remove(min(numbers))
+#     return result
+#
+# print(remove_smallest([1,2,3,4,5]))
+
+
