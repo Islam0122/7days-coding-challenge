43cabff6a23179caeaa80ca543a97179845539ff
islam0122 <duishobaevislam01@gmail.com>
2025-08-28 23:11:22 +0600

    day_04

--- day_04.py
+++ day_04.py
@@ -21,3 +21,27 @@ Beginner - Reduce but Grow --> 8kyu
 #         res = i * res
 #     return res
 
+"""
+Square(n) Sum --> 8kyu
+"""
+# def square_sum(numbers):
+#     res = 0
+#     for num in numbers:
+#         res += num**2
+#     return res
+#
+# print(square_sum([1, 2, 2]))
+
+"""
+Break camelCase --> 6kyu
+"""
+# def solution(s):
+#     result = ""
+#     for char in s:
+#         if char.isupper():
+#             result += " " + char
+#         else:
+#             result += char
+#     return result
+#
+# print(solution("cmmalCammelRose"))
