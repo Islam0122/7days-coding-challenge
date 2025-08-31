6d76015ce665168bc9f8894f40c554106862214a
islam0122 <duishobaevislam01@gmail.com>
2025-08-31 22:16:32 +0600

    day_07

---
+++ day_07.py
@@ -0,0 +1,9 @@
+"""
+Vowel Count --> 7kyu
+"""
+# def get_count(s: str) -> int:
+#     vowels = "aeiou"
+#     return sum(1 for char in s if char in vowels)
+#
+# # Example
+# print(get_count("hello world"))  # Output: 3 (e, o, o)
